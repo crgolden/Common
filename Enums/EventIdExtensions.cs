d55380f89a89179e5f029de772d0b1da9553a9f2
Chris Golden <crgolden@msn.com>
2019-10-24 03:35:55 -0500

    Adding `EventIdExtensions`

---
+++ Enums/EventIdExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Common
+{
+    using System;
+    using System.Linq.Expressions;
+    using Microsoft.Extensions.Logging;
+    using static Moq.It;
+
+    public static class EventIdExtensions
+    {
+        public static Expression<Action<ILogger>> IsLoggedWith(
+            this EventId eventId,
+            LogLevel logLevel,
+            Exception? exception = default)
+        {
+            return logger => logger.Log(
+                logLevel,
+                Is<Microsoft.Extensions.Logging.EventId>(y => y.Id == (int)eventId && y.Name == $"{eventId}"),
+                IsAny<IsAnyType>(),
+                exception,
+                (Func<IsAnyType, Exception, string>)IsAny<object>());
+        }
+    }
+}
