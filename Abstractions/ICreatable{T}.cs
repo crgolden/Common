a15b9e75b49402e26a313534f0339037a07c7943
Chris Golden <crgolden@msn.com>
2020-02-13 06:32:05 -0600

    Adding `ICreatable`

---
+++ Abstractions/ICreatable{T}.cs
@@ -0,0 +1,19 @@
+﻿namespace Common
+{
+    using System;
+    using JetBrains.Annotations;
+
+    /// <summary>An instance that can be created.</summary>
+    /// <typeparam name="T">The type of the identifier.</typeparam>
+    [PublicAPI]
+    public interface ICreatable<T>
+    {
+        /// <summary>Gets or sets the created by.</summary>
+        /// <value>The created by.</value>
+        T CreatedBy { get; set; }
+
+        /// <summary>Gets or sets the created date.</summary>
+        /// <value>The created date.</value>
+        DateTime CreatedDate { get; set; }
+    }
+}
